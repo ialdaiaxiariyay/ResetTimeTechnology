354fbe949a3318d3cea3dbafb2901acf48d8e505
ialdaiaxiariyay <ialdaiaxiariyay@qq.com>
2025-03-08 13:25:00 +0800

    修复一下bug

--- src/main/java/top/ialdaiaxiariyay/rttgtcore/config/RTTGTConfigHolder.java
+++ src/main/java/top/ialdaiaxiariyay/rttgtcore/config/RTTGTConfigHolder.java
@@ -24,7 +24,7 @@ public class RTTGTConfigHolder {
     @Configurable.Comment("开启湛紫微光和微光主机合成表的注册，注意这可能会破坏游戏平衡性")
     public boolean EnableDeepvioletgleamAndCircuitRecipe = false;
     @Configurable
-    @Configurable.Comment("开启通用蒸汽机\n#提示:这可能会有点小OP")
+    @Configurable.Comment("开启通用蒸汽机，这可能会有点小OP")
     public boolean EnableTheGeneralSteamEngine = false;
     @Configurable
     @Configurable.Comment("开启方便AE配方，比较OP")
